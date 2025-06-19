3606632908ec3db8ab1ea0a668f81101f1a46f26
Deneth  Godakandagei <contact.deneth@gmail.com>
2025-06-19 08:13:37 +0530

    set up Apollo client in react app

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+import { useQuery,useMutation, gql } from '@apollo/client'
+
+const GET_USERS =gql`
+  query GetUsers {
+    getUsers {
+     id
+    name
+    age
+    isMarried
+    }
+  }
+`;
+ 
+function App() {
+ 
+  const {data,error ,loading} =useQuery(GET_USERS); 
+
+  if (loading) return <p>Data loading</p>;
+
+  if (error) return <p>Error :{error.message}</p>;
+
+  return (
+    <>
+      <div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
--- server/server.js
+++ server/server.js
@@ -1,54 +1,52 @@
 import { ApolloServer } from "@apollo/server";
-import {startStandaloneServer} from "@apollo/server/standalone"
+import { startStandaloneServer } from "@apollo/server/standalone";
 
 const users = [
-    {id:"1", name:"Deneth" , age:30 ,isMarried:true},
-     {id:"2", name:"kavindu" , age:30 ,isMarried:false},
-      {id:"3", name:"Lakmal" , age:30 ,isMarried:true},
-]
+  { id: "1", name: "Deneth", age: 30, isMarried: true },
+  { id: "2", name: "Kavindu", age: 30, isMarried: false },
+  { id: "3", name: "Lakmal", age: 30, isMarried: true },
+];
 
 const typeDefs = `
-        type Query{
-            getUsers:[User]
-            getUserById(id:Id!):User
-        }
-        type Mutation {
-            createUser(name:String!, age:Int!,isMarried: Boolean!):User
-        }
-        type User{
-            id:Id
-            name:String
-            age:Int
-            isMarried: Boolean
-        }
-`
-const resolvers ={
-    Query:{
-        getUsers:() =>{
-            return users;
-        },
-    getUserById: (parent, args) =>{
-        const id = args.id;
-        return users.find((user) =>user.id===id);
-    },
-    },
-    Mutation:{
-        CreateUser:(parent,args) =>{
-            const {name ,age ,isMarried}=args
-            const newUser = {
-                id:(users.length+1).toString(),
-                name,
-                age,
-                isMarried,
-            };
-            users.push(newUser);
-        }
-    }
-}
+  type Query {
+    getUsers: [User]
+    getUserById(id: ID!): User
+  }
+  type Mutation {
+    createUser(name: String!, age: Int!, isMarried: Boolean!): User
+  }
+  type User {
+    id: ID
+    name: String
+    age: Int
+    isMarried: Boolean
+  }
+`;
 
-const server = new ApolloServer({typeDefs,resolvers});
+const resolvers = {
+  Query: {
+    getUsers: () => users,
+    getUserById: (parent, args) => users.find((user) => user.id === args.id),
+  },
+  Mutation: {
+    createUser: (parent, args) => {
+      const { name, age, isMarried } = args;
+      const newUser = {
+        id: (users.length + 1).toString(),
+        name,
+        age,
+        isMarried,
+      };
+      users.push(newUser);
+      return newUser;
+    },
+  },
+};
 
-const {} = await startStandaloneServer(server,{
-    listen: {port:4000},
-})
-console.log('server running')
+const server = new ApolloServer({ typeDefs, resolvers });
+
+await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log("Server running at http://localhost:4000/");
