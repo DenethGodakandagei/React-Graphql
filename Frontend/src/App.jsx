0d2a2f2bc9bcadc0effe85f098cfec2d62561dec
Deneth  Godakandagei <contact.deneth@gmail.com>
2025-06-20 11:02:07 +0530

    get user by id

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useQuery, useMutation, gql } from "@apollo/client";
+import { useQuery, gql } from "@apollo/client";
 
 const GET_USERS = gql`
   query GetUsers {
@@ -12,25 +12,61 @@ const GET_USERS = gql`
   }
 `;
 
+const GET_USER_BY_ID = gql`
+  query GetUserById($id: ID!) {
+    getUserById(id: $id) {
+      id
+      name
+      age
+      isMarried
+    }
+  }
+`;
+
 function App() {
-  const { data, error, loading } = useQuery(GET_USERS);
+  
+  const userId = "1"; 
 
-  if (loading) return <p>Data loading</p>;
+  const {
+    data: getUserByIdData,
+    error: getUserByIdError,
+    loading: getUserByIdLoading,
+  } = useQuery(GET_USER_BY_ID, {
+    variables: { id: userId },
+    skip: !userId, 
+  });
 
-  if (error) return <p>Error :{error.message}</p>;
+  const {
+    data: getUsersData,
+    error: getUsersError,
+    loading: getUsersLoading,
+  } = useQuery(GET_USERS);
+
+  if (getUsersLoading) return <p>Loading users...</p>;
+  if (getUsersError) return <p>Error loading users: {getUsersError.message}</p>;
 
   return (
-    <>
-      <div>
-        <h1>Users</h1>
-        {data.getUsers.map((user) => (
-          <div className="flex ">
-            <p> Name : {user.name}</p>
+    <div>
+      <h1>Chosen User:</h1>
+    {getUserByIdLoading ? (
+  <p>Loading selected user...</p>
+) : getUserByIdError ? (
+  <p>Error loading selected user: {getUserByIdError.message}</p>
+) : (
+  getUserByIdData?.getUserById && (
+    <p>Name: {getUserByIdData.getUserById.name}</p>
+  )
+)}
 
-          </div>
-        ))}
-      </div>
-    </>
+
+      <h1>All Users</h1>
+      {getUsersData.getUsers.map((user) => (
+        <div className="flex" key={user.id}>
+          <p>Name: {user.name}</p>
+          {user.id}
+        </div>
+      ))}
+    </div>
   );
 }
 
